8bbec16ddcd0bca4166a2407b0838a9429fb870f
thepassle <pascalschilp@gmail.com>
2024-01-20 21:04:57 +0100

    cut out @web/mocks dep, use only msw directly

--- test.js
+++ test.js
@@ -1,9 +1,17 @@
-import { registerMockRoutes } from '@web/mocks/browser.js';
-import { http } from '@web/mocks/http.js';
+import { http } from 'msw';
+import { setupWorker } from 'msw/browser';
+
+const worker = setupWorker();
+worker
+  .start({
+    serviceWorker: {
+      url: './mockServiceWorker.js',
+    },
+  });
 
 describe('test', () => {
   it('api', async () => {
-    registerMockRoutes(http.get('/api/foo', () => Response.json({ foo: 'foo' })));
+    worker.use(http.get('/api/foo', () => Response.json({ foo: 'foo' })));
 
     const response = await fetch('/api/foo').then(r => r.json());
     if(response.foo !== 'foo') {
--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,8 +1,5 @@
-import { mockPlugin } from "@web/mocks/plugins.js";
-
-// @see https://modern-web.dev/docs/test-runner/cli-and-configuration/#configuration-file
 export default {
   nodeResolve: true,
   files: ["test.js"],
-  plugins: [mockPlugin()],
+  // plugins: [mockPlugin()],
 };
